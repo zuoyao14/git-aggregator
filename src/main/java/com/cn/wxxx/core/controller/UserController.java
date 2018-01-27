c67667dda43daebf3794ab494fa92c41f949d7e9
Administrator <Administrator@XTZ-01711240920>
2018-01-27 15:01:25 +0800

    控制层开发

---
+++ src/main/java/com/cn/wxxx/core/controller/UserController.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.cn.wxxx.core.controller;
+
+/**
+ * 功    能：文章接口类
+ *<br/>
+ *Allrights reserved.
+ *<br/>
+ *项目名：大潮后台管理admin-系统框架-数据持久层-DAO	层 
+ *<br/>
+ *文件名：com.cn.wxxx.core.controller.UserController.java
+ *<br/>
+ *作    者：ZUOYAO
+ *<br/>
+ *日    期：2018年1月27日	下午2:47:54
+ *<br/>
+ *摘    要:
+ *<br/>
+ *JDK:1.8
+ *<br/>
+ *Copyright ( c ) 2018 北京大潮研学国际旅行社有限公司
+ */
+
+/**
+ * @author Administrator
+ *
+ */
+public class UserController {
+
+	/**
+	 * 
+	 */
+	public UserController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
